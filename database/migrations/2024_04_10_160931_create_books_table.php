3b42f926e8aaf00120be96663c94cf21590e90ea
mryeminaung <yeminaung013@gmail.com>
2024-04-10 23:41:40 +0630

    revise database migrations

---
+++ database/migrations/2024_04_10_160931_create_books_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id('book_id');
+            $table->string('author')->comment('author\'s name');
+            // unique method ထဲက name က custom index key ဖြစ်
+            $table->string('email')->unique('my_email');
+            $table->double('price');
+            $table->enum('level', ['easy', 'medium', 'hard']);
+            $table->boolean('isStock')->default(true);
+            $table->date('published_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
